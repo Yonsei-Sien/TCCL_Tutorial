67bd9a73cedfda799e987fa6133b73ece12caba8
Yonsei-Sien <144093191+Yonsei-Sien@users.noreply.github.com>
2023-11-13 16:44:25 +0900

    Update tutorial_header.py

--- tutorial_header.py
+++ tutorial_header.py
@@ -71,6 +71,7 @@ class DFT:
                         mol_hf.diis_space=40
                         mol_sc.conv_tol=1e-8
                         mol_hf.diis=scf.ADIIS()
+                        mol_hf.max_cycle=500
                         mol_hf.kernel()
                         Dhf=mol_hf.make_rdm1()
                         Ehf=mol_sc.energy_tot(Dhf)
