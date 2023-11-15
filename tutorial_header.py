a27cce2d86f59ac10ca33e8cab2bc4e957a93bdb
Yonsei-Sien <144093191+Yonsei-Sien@users.noreply.github.com>
2023-11-15 15:25:32 +0900

    Update tutorial_header.py

--- tutorial_header.py
+++ tutorial_header.py
@@ -38,7 +38,7 @@ class DFT:
                         mol_sc.conv_check=False
                         mol_sc.grids.level=4
                         mol_sc.conv_tol=1e-8
-                        #mol_sc.diis=scf.ADIIS()
+                        mol_sc.diis=scf.ADIIS()
                         mol_sc.diis_space=40
                         mol_sc.max_cycle=max_cycle
 
@@ -66,14 +66,14 @@ class DFT:
                         mol_sc.grids.level=4
                         mol_sc.conv_tol=1e-8
                         mol_sc.diis_space=40
-                        #mol_sc.diis=scf.ADIIS()
+                        mol_sc.diis=scf.ADIIS()
                         mol_sc.diis_space=40
                         
                         mol_hf=scf.UHF(mol).density_fit()
                         mol_hf.diis_space=40
                         mol_hf.conv_tol=1e-8
                         mol_hf.max_cycle=500
-                        #mol_hf.diis=scf.ADIIS()
+                        mol_hf.diis=scf.ADIIS()
                         mol_hf.diis_space=40
                         mol_hf.kernel()
                         Dhf=mol_hf.make_rdm1()
